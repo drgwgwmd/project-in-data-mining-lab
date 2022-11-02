69c3d0373139174f9ac26bff6f9f154f03ced4d4
Omer <Omer@OMER>
2022-11-02 19:34:50 +0200

    After bug fix (time taken was always 0 milisecs)

--- src/TestThreadCheckArray.java
+++ src/TestThreadCheckArray.java
@@ -18,20 +18,18 @@ public class TestThreadCheckArray {
 			System.out.println("Enter number");
 			num = input.nextInt();
 			
-			SharedData sd = new SharedData(array, num);
+			//Start counting the time it'll take
+			Instant start = Instant.now();
 			
+			SharedData sd = new SharedData(array, num);
 			thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
 			thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
-			Instant start = Instant.now();
 			thread1.start();
 			thread2.start();
 			try 
 			{
 				thread1.join();
 				thread2.join();
-				Instant end = Instant.now();
-				Duration timeElapsed = Duration.between(start, end);
-				System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
 			}
 			catch (InterruptedException e)
 			{
@@ -42,6 +40,12 @@ public class TestThreadCheckArray {
 				System.out.println("Sorry");
 				return;
 			}
+			//Counting the time it took
+			Instant end = Instant.now();
+			Duration timeElapsed = Duration.between(start, end);
+			//Printing the time it took
+			System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
+			
 			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().size());
 			System.out.print("I:    ");
 			for(int index = 0; index < sd.getArray().size() ; index++)
